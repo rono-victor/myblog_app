3c792cc060db2e03748772673c2b6b4741ed63b8
vvronc <ronovictor40@gmail.com>
2023-11-08 09:46:48 +0300

    added list and detail views

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -1,8 +1,15 @@
 from django.db import models
+from django.db.models.query import QuerySet
 from django.utils import timezone
 from django.contrib.auth.models import User
 
 # Create your models here.
+class PublishedManager(models.Manager):
+    def get_queryset(self):
+        return super().get_queryset()\
+        .filter(status=Post.Status.PUBLISHED)
+
+
 class Post (models.Model):
 
     class Status(models.TextChoices):
@@ -21,6 +28,10 @@ class Post (models.Model):
     Status = models.CharField(max_length=2,
                               choices= Status.choices,
                               default= Status.DRAFT)
+
+    objects=models.Manager()#default manager
+    published = PublishedManager()#our custom manager
+
 
     class Meta:
         ordering = ['-publish']
