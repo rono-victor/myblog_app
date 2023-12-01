1d2a831b011814a8945c9fae391d387371dda2b6
vvronc <ronovictor40@gmail.com>
2023-12-01 09:55:53 +0300

    feeds

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -26,5 +26,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('blog/', include('blog.urls', namespace='blog')),
     path('sitemap.xml', sitemap, {'sitemaps': sitemaps},
-         name='django.contrib.sitemaps.views.sitemap')
+         name='django.contrib.sitemaps.views.sitemao')
 ]
